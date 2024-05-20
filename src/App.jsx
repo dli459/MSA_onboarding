58514187106f1f4fdf1efe69e357ea2c49b9fdc8
Kitty Li <55778381+dli459@users.noreply.github.com>
2024-05-20 03:30:30 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Danlei Li",
+  title: "Future-driven AI Algorithm Engineer",
+  email: "kitty.dmr.li@gmail.com",
+  gitHub: "https://github.com/dli459",
+  linkedIn: "https://www.linkedin.com/in/danlei-li/",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Proud sister & Future-driven AI Algorithm Engineer & Computer System Engineering PhD Student at University of Auckland & Specializing in Machine Learning, Deep Learning, Data Mining and Anomaly Detection";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Kitty Li received a Bachelor of Science (Hons.) degree in Computer Science and a master's degree in Computer Science from the Department of Science, the University of Auckland, New Zealand, in 2016 and 2021, respectively. She is currently a PhD student in Computer System Engineering at the University of Auckland. She is familiar with machine learning and especially anomaly detection techniques. Her current research focuses on anomaly detection for sensor time series.";
 
 const About = () => {
   return (
